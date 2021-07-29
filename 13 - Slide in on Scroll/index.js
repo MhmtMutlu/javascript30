e80f4a20ad60d921f1518f418272fee7cb3c539f
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-07-29 16:53:32 +0300

    Thirteenth project was completed.

---
+++ 13 - Slide in on Scroll/index.js
@@ -0,0 +1,35 @@
+
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImagesDOM = document.querySelectorAll(".slide-in");
+
+function slide(e) {
+  sliderImagesDOM.forEach(sliderImage => {
+    // half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add('active');
+    } else {
+      sliderImage.classList.remove('active');
+    }
+  })
+}
+
+window.addEventListener("scroll", debounce(slide))
